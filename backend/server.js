c4f78893960eb4fe6db891b36c507fdaf279e54d
Ashim Dutta <ashimdutta800@gmail.com>
2025-07-31 23:04:19 +0530

    dist file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
+const express = require("express");
 const app = require('./src/app')
 const connectDB = require('./src/db/db')
+const path = require("path");
 
 
-
+app.use(express.static(path.join(__dirname, "../frontend/dist"))); 
 connectDB()
 app.listen(3000, () => { 
     console.log("server is running on port");
